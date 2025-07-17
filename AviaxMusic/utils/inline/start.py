621316fb8b446583f703e201dca1da9ceb2c2d3b
DREK <userbot870@gmail.com>
2025-07-17 23:25:05 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -25,7 +25,7 @@ def private_panel(_):
             )
         ],
         [
-            IlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
         [
